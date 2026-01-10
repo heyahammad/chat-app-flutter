26c01ab113b07bb7420bc221c001e930804b1aea
MD. FAISAL AHAMMAD <fahad.ahmed2456@gmail.com>
2026-01-11 03:34:46 +0600

    home page: initial setup

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:say/screen/auth_screen.dart';
+
+final user = FirebaseAuth.instance.currentUser;
+final store = FirebaseFirestore.instance;
 
 class HomneScreen extends StatefulWidget {
   const HomneScreen({super.key});
@@ -8,8 +15,69 @@ class HomneScreen extends StatefulWidget {
 }
 
 class _HomneScreenState extends State<HomneScreen> {
+  String firstname = '';
+  String lastname = '';
+  String username = '';
+  DateTime? dob;
+  String mail = '';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    Widget content = Container(
+      decoration: BoxDecoration(
+        color: 
+      ),
+      child: Center(child: Text(mail)));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 5.0),
+          child: Image.asset('assets/logo.png', width: 100),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 5.0),
+            child: IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.logout_outlined,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: store.collection('users').doc(user!.uid).snapshots(),
+        builder: (ctx, snapshots) {
+          if (snapshots.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            );
+          }
+          if (snapshots.hasData) {
+            final userData = snapshots.data!.data() as Map<String, dynamic>;
+            firstname = userData['firstname'];
+            lastname = userData['lastname'];
+            username = userData['username'];
+            dob = DateTime.parse(userData['dob']);
+            mail = user!.email!;
+
+            return content;
+          }
+          if (snapshots.hasError) {
+            return Center(child: Text('Something went wrong!'));
+          }
+          return Center(child: Text('Something went wrong!'));
+        },
+      ),
+    );
   }
 }
